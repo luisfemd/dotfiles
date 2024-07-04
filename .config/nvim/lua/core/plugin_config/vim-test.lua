44ee854abb49599da3ba41a66035c272bdefff10
Luis Felipe Montoya <luisfemd@gmail.com>
2024-07-03 23:32:31 -0500

    init termux branch

--- .config/nvim/lua/core/plugin_config/vim-test.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd [[
-  let test#strategy = "neoterm"
-]]
-
-vim.keymap.set('n', '<leader>tt', ':TestNearest<CR>')
-vim.keymap.set('n', '<leader>tf', ':TestFile<CR>')
-vim.keymap.set('n', '<leader>tl', ':TestLast<CR>')
-

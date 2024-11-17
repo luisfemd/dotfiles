4d6da183d0b13065d83d388e4dcb4e855d5c6a9e
Luis Felipe Montoya <luisfemd@gmail.com>
2024-11-17 08:43:04 -0500

    changes in nvim

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,7 +1,5 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
-
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -14,6 +14,9 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim", tag = "0.1.4",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
+  { 'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' 
+  },
   {
     "kylechui/nvim-surround",
     version = "*", 
@@ -29,12 +32,57 @@ require("lazy").setup({
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "groovy", "python", "sql" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "eex", "heex", "javascript", "html", "groovy", "python", "sql", "typescript" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },  
       })
     end
   },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			lspconfig.elixirls.setup({
+				cmd = { "elixir-ls" },
+				capabilities = capabilities,
+			})
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			-- install different completion source
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				-- add different completion source
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+				-- using default mapping preset
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				snippet = {
+					-- you must specify a snippet engine
+					expand = function(args)
+						-- using neovim v0.10 native snippet feature
+						-- you can also use other snippet engines
+						vim.snippet.expand(args.body)
+					end,
+				},
+			})
+		end,
+	},
 })
 

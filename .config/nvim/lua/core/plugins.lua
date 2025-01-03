f1f27ff20c1c760c93960ccbd4c6240b15e5c3bb
Luis Felipe Montoya <luisfemd@gmail.com>
2025-01-03 18:10:47 -0500

    add basic configuration for Copilot Chat

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -84,5 +84,29 @@ require("lazy").setup({
 			})
 		end,
 	},
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    keys = {
+      {
+        "<leader>ccq",
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+          end
+        end,
+        desc = "CopilotChat - Quick chat",
+      }
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 })
 

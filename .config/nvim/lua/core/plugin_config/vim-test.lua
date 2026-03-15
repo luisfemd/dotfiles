417bb67bfd3333d9ebdbe1ac293a3f791890d4d1
Luis Felipe Montoya <luisfemd@gmail.com>
2026-03-15 11:40:32 -0500

    add key mapper for custom test in Elixir

--- .config/nvim/lua/core/plugin_config/vim-test.lua
+++ .config/nvim/lua/core/plugin_config/vim-test.lua
@@ -1,5 +1,7 @@
 vim.cmd [[
   let test#strategy = "neoterm"
+  " Use unit_test instead test to avoid drop/create/migrate
+  let test#elixir#exunit#executable = 'MIX_ENV=test mix unit_test'
 ]]
 
 vim.keymap.set('n', '<leader>tt', ':TestNearest<CR>')
